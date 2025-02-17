97031c1c1d73de0712c02c2745eba01c99909d59
Adrien Abbey <abbey.12@wright.edu>
2025-02-16 19:36:25 -0500

    First attempt at creating Docker images on the fly.

---
+++ containers/first-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This bash script is intended to launch my growing stack of Docker containers.
+# TODO: Make this script idempotent.
+#   - Don't create a network if it already exists.
+#   - Recognize and use existing images.  Update these images as needed?
+
+# Create a network for my containers to use:
+docker network create my-network
+
+# Create a Docker volume for persistent storage:
+docker volume create mariadb_data
+docker volume create mediawiki_data
+
+# Run the MariaDB container:
+docker run -d \
+  --name my_mariadb \
+  --net my-network \
+  -e MARIADB_ROOT_PASSWORD=my-secret-pw \
+  -e MARIADB_DATABASE=my_app_db \
+  -e MARIADB_USER=my_user \
+  -e MARIADB_PASSWORD=$(cat mariadb/.mariadb_password) \
+  -v mariadb_data:/var/lib/mysql \
+  mariadb:latest
